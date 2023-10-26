a3c59487eb1fe232312c604a7fad1e796b035391
sudhamsh <sudhamsh1996@gmail.com>
2023-10-26 22:54:19 +0530

    added the terraform import stuff

--- import-terrafrom/main.tf
+++ import-terrafrom/main.tf
@@ -1,6 +1,6 @@
 # terrafrom import <resource-type>.<reference-name> instance-id
 # Terraform can import existing infrastructure resources. This functionality lets you bring existing resources under Terraform management.
-# resource block would be fine
+# resource block would be fine, then add all the necessary attributes you want
 resource "aws_instance" "webserver" {
   ami           = "ami-0571c1aedb4b8c5fc"
   instance_type = "t2.micro"
